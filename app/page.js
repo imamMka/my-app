5eeb4fdf698ff695435b4108374a2bb7a8c35215
amnasj <imammka23@gmail.com>
2025-07-22 16:41:17 +0700

    progres progres

--- app/page.js
+++ app/page.js
@@ -1,103 +1,118 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+export default function home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="bg-[#2d2d2d] text-white flex justify-center items-center">
+      <section className="flex flex-col justify-center items-center h-screen w-[596px]">
+        <div className=" w-full flex justify-between m-4 p-4 items-center border-b border-gray-200">
+          <input
+            type="text"
+            placeholder="Insert your place name"
+            className="w-full"
+          />
+          <img src="/search.svg" alt="" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className=" w-full flex gap-3 m-4 p-4 items-center border-b border-gray-200">
+          <div>
+            <h2 className="font-bold text-[24px]">monday</h2>
+            <p className="text-[24px]">04 September</p>
+          </div>
+          <h1 className="font-bold text-[50px]">JAKARTA</h1>
+        </div>
+
+        <div className="w-full flex gap-2 m-4 p-4 items-center border-b border-gray-200">
+          <div>
+            <h1 className="text-[98px] font-bold">29°</h1>
+            <p className="text-[23px]">Cloudy</p>
+          </div>
+          <img src="/cloudy.svg" alt="" />
+        </div>
+
+        <div className="w-full flex justify-between">
+          <div className="flex gap-2 items-start">
+            <div>
+              <img src="/wind.svg" alt="" />
+              <p className="text-[15px]">Wind</p>
+            </div>
+            <h2 className="text-[25px]">6.69/ms</h2>
+          </div>
+
+          <div className="flex gap-2 items-start">
+            <div>
+              <img src="/humidity.svg" alt="" />
+              <p className="text-[15px]">Humidity</p>
+            </div>
+            <h2 className="text-[25px]">70%</h2>
+          </div>
+
+          <div className="flex gap-2 items-start ">
+            <div>
+              <img src="/cloud.svg" alt="" />
+              <p className="text-[15px]">Clouds</p>
+            </div>
+            <h2 className="text-[25px]">40%</h2>
+          </div>
+
+          <div className="flex gap-2 items-start ">
+            <div>
+              <img src="/thermo.svg" alt="" />
+              <p className="text-[15px]">Real feel</p>
+            </div>
+            <h2 className="text-[25px]">40%</h2>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-4 mt-4 items-start">
+          <h2 className="text-[21px]">Weekly Forecast</h2>
+          <div className="flex justify-between w-full">
+            <div className="bg-black/35 w-[100px] p-2.5">
+              <p>Tuesday</p>
+              <div className="flex gap-2 items-center">
+                <img src="/cloudy.svg" alt="" className="w-[28px]" />
+                <p className="text-[22px]">26°</p>
+              </div>
+              <p>Cloudy</p>
+            </div>
+
+            <div className="bg-black/35 w-[100px] p-2.5">
+              <p>Wednesday</p>
+              <div className="flex gap-2 items-center">
+                <img src="/rainy.svg" alt="" className="w-[28px]" />
+                <p className="text-[22px]">14°</p>
+              </div>
+              <p>Rainy</p>
+            </div>
+
+            <div className="bg-black/35 w-[100px] p-2.5">
+              <p>Thursday</p>
+              <div className="flex gap-2 items-center">
+                <img src="/cloudy.svg" alt="" className="w-[28px]" />
+                <p className="text-[22px]">25°</p>
+              </div>
+              <p>Cloudy</p>
+            </div>
+
+            <div className="bg-black/35 w-[100px] p-2.5">
+              <p>Friday</p>
+              <div className="flex gap-2 items-center">
+                <img src="/sun.svg" alt="" className="w-[28px]" />
+                <p className="text-[22px]">32°</p>
+              </div>
+              <p>Clear</p>
+            </div>
+
+            <div className="bg-black/35 w-[100px] p-2.5">
+              <p>Saturday</p>
+              <div className="flex gap-2 items-center">
+                <img src="/cloudy.svg" alt="" className="w-[28px]" />
+                <p className="text-[22px]">22°</p>
+              </div>
+              <p>Cloudy</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
